c93d2fc0c817a41e9b515fb6a74c57c705c8a0fa
ItzRafaelScout <rafaelaltrafaelduartealt@gmail.com>
2025-03-24 19:35:06 -0300

    Update script.js

--- cilent/script.js
+++ cilent/script.js
@@ -1,12 +1,10 @@
 $(document).ready(function() {
-    let $template = $("#welcome-template");
+    let $template = $("#user-template");
     
     if ($template.length > 0 && $template[0].content) {
         let welcomeBodyContent = document.importNode($template[0].content, true);
         let $mainContent = $("#main-content");
         $mainContent.empty();
         $mainContent.append(welcomeBodyContent);
-    } else {
-        console.error("Template not found or not a valid template element");
     }
 });
